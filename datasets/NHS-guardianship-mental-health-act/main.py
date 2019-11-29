ae198a177b6726a760e84b9dc3a06cb0d3aadc74
Alex Tucker <alex@floop.org.uk>
2019-11-29 08:05:54 +0000

    Check scraper works from landingPage

---
+++ datasets/NHS-guardianship-mental-health-act/main.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+from gssutils import *
+import json
+
+info = json.load(open('info.json'))
+scraper = Scraper(info['landingPage'])
+scraper
+# -
+
+
