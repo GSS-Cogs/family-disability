fdb28502bdb82b76a6f9d6638953c2a56322ce7c
LPerryman <leigh.perryman@gsscogs.uk>
2019-11-29 12:25:19 +0000

    Adding Marker to comnponents.csv file

--- datasets/DWP-Family-Resources-Survey-Carer/main.py
+++ datasets/DWP-Family-Resources-Survey-Carer/main.py
@@ -429,10 +429,10 @@ def changeDataMarkerValues(tbl):
     try:
         colName = 'DATAMARKER'
         if colName in tbl.columns:
-            tbl[colName][tbl[colName] == '0'] = 'Nil none recorded in the sample)'
-            tbl[colName][tbl[colName] == '-'] = 'Negligible (less than 0.5 percent, or 0.1 million)'
-            tbl[colName][tbl[colName] == '.'] = 'N/A'
-            tbl[colName][tbl[colName] == '..'] = 'Not available due to small smaple size (fewer than 100)'
+            tbl[colName][tbl[colName] == '0'] = 'Nil none recorded in the sample'
+            tbl[colName][tbl[colName] == '-'] = 'Negligible less than 0.5 percent or 0.1 million'
+            tbl[colName][tbl[colName] == '.'] = 'Not applicable'
+            tbl[colName][tbl[colName] == '..'] = 'Not available due to small sample size fewer than 100'
         
         tbl = tbl.rename(columns={colName:'Marker'})
         return tbl
