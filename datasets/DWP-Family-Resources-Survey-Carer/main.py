4d469332ac879250fe4032e53ac0ca416fc77599
LPerryman <leigh.perryman@gsscogs.uk>
2019-12-05 12:09:02 +0000

    changing percent to percentage in main.py

--- datasets/DWP-Family-Resources-Survey-Carer/main.py
+++ datasets/DWP-Family-Resources-Survey-Carer/main.py
@@ -533,7 +533,7 @@ for t in tblSet:
     
     if measType in t.columns:
         #t[measType] = t[measType].str.replace(' ', '-')
-        t[measType] = 'Percent'
+        t[measType] = 'Percentage'
         t[measType] = t[measType].apply(pathify)
     
     if ageHead in t.columns:
