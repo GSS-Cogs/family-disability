d574a7cbf7361e755af26b21b99dd1c0656ef0bd
LPerryman <45283878+LPerryman@users.noreply.github.com>
2019-12-06 12:59:28 +0000

    Update main.py

--- datasets/DWP-Family-Resources-Survey-Carer/main.py
+++ datasets/DWP-Family-Resources-Survey-Carer/main.py
@@ -95,8 +95,8 @@ def extract_sheet_5_1_and_5_2_and_5_8(tab, mainCol, whichTab, gHeading, yrRange,
         if 'DATAMARKER' not in tbl.columns:
             tbl['DATAMARKER'] = 'not-applicable'
         
-        tbl = tbl[[yrRange,ageH,gHeading,'Measure Type','Sample Size','DATAMARKER','Value','Unit']]
-        #tbl = tbl[[ageH,'Value','Unit','Measure Type','Sample Size']]
+        #tbl = tbl[[yrRange,ageH,gHeading,'Measure Type','Sample Size','DATAMARKER','Value','Unit']]
+        bl = tbl[[yrRange,ageH,gHeading,'Sample Size','Measure Type','Value','Unit','DATAMARKER']]
 
         return tbl
     except Exception as e:
