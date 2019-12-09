041fe701c5c5afb1225a6835a935ca365f87894d
VTula2000 <36479963+VTula2000@users.noreply.github.com>
2019-12-09 16:51:24 +0000

    Update dimension

--- datasets/NHS-measures-adult-social-care-outcomes/main.py
+++ datasets/NHS-measures-adult-social-care-outcomes/main.py
@@ -21,25 +21,58 @@ dist
 table = dist.as_pandas(encoding='Windows-1252')
 table
 
-# %%
-table.rename(columns={
-    'Geographical Code': 'NHS Geography',
-    'ONS Area Code': 'ONS Geography'
-}, inplace=True)
-table.drop(columns=['Geographical Description', 'Geographical Level'], inplace=True)
-table
+# %% [markdown]
+#  'Geographical Description', 'Geographical Level' removed as those covered by 'Geographical Code'
+#  'ONS Area Code' removed as data set relevant code kept in tidy data, and also based on 
+#  https://files.digital.nhs.uk/19/C45DF7/meas-from-asc-of-eng-1819-Appendices.pdf due to variation in both geographical codes, only `Geographical Code` in tidy data 
 
 # %%
-for col in table:
-    if col not in 'Measure Value':
-        table[col] = table[col].astype('category')
-        display(col)
-        display(table[col].cat.categories)
+table.rename(columns={
+    'Geographical Code': 'NHS Geography'    
+}, inplace=True)
+table.drop(columns=['Geographical Description', 'Geographical Level','ONS Area Code', 
+                    'Measure Group Description'], inplace=True)
+
+# %% [markdown]
+# Here ideal Tidy data should look like this with dimensions
+# `NHS Geography,	ASCOF Measure Code,	Measure Group Description, Measure Type, Value, CI, Count`
+# But data have various missing values for ASCOF Codes, not all Outcome have Margin Of Error , 
+# Here ASCOF codes were more than observations, Outcome are 17868 and Margin Of Error are only 10126, so significant amount of data missing.
+
+# %%
+nt1 = table[table['Measure Type'] == 'Outcome']
+
+# %%
+nt1.columns = ['Value' if x=='Measure Value' else x for x in table.columns]
+
+# %%
+nt2 = table[table['Measure Type'] == 'Margin Of Error']
+
+# %%
+nt2.columns = ['CI' if x=='Measure Value' else x for x in table.columns]
+
+# %%
+nt2.drop(columns=['Measure Type', 'Disaggregation Level','Measure Group'], inplace=True)
+
+# %%
+Final_table = pd.merge(nt1, nt2, how = 'outer', on = ['NHS Geography','ASCOF Measure Code'])
+
+# %%
+Final_table = Final_table[Final_table['Value'].isnull() == False]
+
+# %% [markdown]
+# 'c' not defined in source data
+
+# %%
+Final_table = Final_table[Final_table['Value'] != 'c' ]
+
+# %%
+Final_table = Final_table[['NHS Geography','ASCOF Measure Code','Disaggregation Level','Measure Group','Measure Type','Value','CI']]
 
 # %%
 out = Path('out')
 out.mkdir(exist_ok=True, parents=True)
-table.to_csv(out / 'observations.csv', index = False)
+Final_table.to_csv(out / 'observations.csv', index = False)
 
 
 # %%
@@ -52,5 +85,3 @@ with open(out / 'observations.csv-metadata.trig', 'wb') as metadata:
 # %%
 csvw = CSVWMetadata('https://gss-cogs.github.io/family-disability/reference/')
 csvw.create(out / 'observations.csv', out / 'observations.csv-schema.json')
-
-# %%
