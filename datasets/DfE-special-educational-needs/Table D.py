f87b1bea2d2f09139fde361857cc9054efdcb074
VTula2000 <36479963+VTula2000@users.noreply.github.com>
2019-12-13 17:41:01 +0000

    Support type code update

--- datasets/DfE-special-educational-needs/Table D.py
+++ datasets/DfE-special-educational-needs/Table D.py
@@ -15,6 +15,7 @@
 
 # State-funded primary, secondary and special schools (1,2): Pupils with special educational needs by age (3) and gender (4,5)
 
+# +
 from gssutils import *
 scraper = Scraper('https://www.gov.uk/government/collections/statistics-special-educational-needs-sen')
 scraper.select_dataset(title=lambda x: x.startswith('Special educational needs in England'), latest=True)
@@ -23,6 +24,7 @@ tab = tabs['Table D']
 cell = tab.filter('England')
 pupils = cell.fill(DOWN).is_not_blank().is_not_whitespace()
 support = cell.shift(0,2).fill(RIGHT).is_not_blank().is_not_whitespace()
+support1 = support.shift(0,1).expand(RIGHT).is_not_blank().is_not_whitespace()
 obs = cell.shift(0,3).fill(RIGHT).is_not_blank().is_not_whitespace() \
         .filter(lambda x: type(x.value) != '%' not in x.value)
 observations1 = obs.fill(DOWN).is_number().is_not_blank().is_not_whitespace() 
@@ -32,7 +34,8 @@ Dimensions1 = [
             HDimConst('Education provider','state-funded-primary-secondary-and-special-schools'),
             HDimConst('Unit','children'),  
             HDimConst('Measure Type','Count'),
-            HDim(pupils, 'Special need type', DIRECTLY, LEFT),
+            HDim(support1, 'support', CLOSEST, LEFT),
+            HDim(pupils, 'Special need type', DIRECTLY,LEFT),
             HDim(support,'Special support type', CLOSEST,LEFT),
             HDimConst('Sex','all'),
             HDimConst('Age','all')            
@@ -43,6 +46,16 @@ import numpy as np
 new_table.rename(columns={'OBS': 'Value'}, inplace=True)
 new_table['Value'] = new_table['Value'].astype(int)
 new_table['Special support type'] = new_table['Special support type'].str.lower()
-new_table['Special support type'] = new_table['Special support type'] + 'eligible-and-claiming-free-school-meals'
+new_table['Special support type'] = new_table['support'] + ' ' + new_table['Special support type']
+new_table['Special support type'] = new_table['Special support type'].map(
+                                    lambda x: {'All Pupils pupils on sen support' : 'All pupils on sen support',
+       'Number of pupils known to be eligible for and claiming free school meals pupils on sen support' : \
+        'Number of pupils known to be eligible for and claiming free school meals on sen support',
+          'All Pupils(5) pupils with sen with statements or ehc plan' : 'All pupils with sen with statements or ehc plan',          
+             'Number of pupils known to be eligible for and claiming free school meals pupils with sen with statements or ehc plan' :\
+             'Number of pupils known to be eligible for and claiming free school meals with sen with statements or ehc plan'                                       
+                                        
+                                        }.get(x, x))
+
 new_table = new_table [['Geography','Period','Education provider','Special support type', 'Special need type','Age','Sex','Unit','Value','Measure Type']]
 
