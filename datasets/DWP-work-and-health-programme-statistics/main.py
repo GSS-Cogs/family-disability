f6fd2313421d02457dfbf7b482ba3f46c123e1a2
RedWalters <jonathan.j.walters@Gmail.com>
2020-01-15 14:40:11 +0000

    Splitting Tabs
    Splitting the Tabs into Named Tables to make the transformed data clearer to viewers (as advised)

--- datasets/DWP-work-and-health-programme-statistics/main.py
+++ datasets/DWP-work-and-health-programme-statistics/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[79]:
+# In[50]:
 
 
 from gssutils import *
@@ -23,16 +23,19 @@ scraper = Scraper('https://www.gov.uk/government/statistics/work-and-health-prog
 scraper
 
 
-# In[80]:
+# In[51]:
 
 
 dist = scraper.distribution(title=lambda t: 'Tables' in t)
 tabs = (t for t in dist.as_databaker())
 tidied_sheets = []
+tidy_tabs = {}
 
 for tab in tabs:
     
     if tab.name in ['1_1']:
+        
+        tab_name = 'Number of Referrals, Individuals Referred and Starts by month of referral by Referral Group'
         
         cell = tab.filter("In month")
         
@@ -63,8 +66,13 @@ for tab in tabs:
         savepreviewhtml(c1, fname="Preview.html")
         tidied_sheets.append(c1.topandas())
         
+        new_table_1_1 = pd.concat(tidied_sheets, ignore_index = True, sort = True).fillna('')
+        
+        tidy_tabs[tab_name] = new_table_1_1
         
     elif tab.name in ['1_2']:
+        
+        tab_name = 'Number of Starts and Job Outcomes achieved within 6 and 12 months of starting, by Start month by Referral Group'
         
         cell = tab.filter("In month")
         
@@ -72,13 +80,11 @@ for tab in tabs:
         
         period = cell.shift(2,0).fill(DOWN).is_not_blank().shift(-2,0) - remove
 
-        referralGroup = cell.shift(5,0).fill(RIGHT).is_not_blank()
-        
-        remove2 = referralGroup.filter('Total').expand(DOWN)
+        referralGroup = cell.fill(RIGHT).is_not_blank()
         
         obsType = cell.shift(1,-1).fill(RIGHT).is_not_blank()
 
-        observations = period.shift(5,0).fill(RIGHT).is_not_blank() - remove - remove2
+        observations = period.fill(RIGHT).is_not_blank() - remove
 
         dimensions = [
                 HDim(period, 'Period', DIRECTLY, LEFT), 
@@ -95,8 +101,13 @@ for tab in tabs:
         savepreviewhtml(c1, fname="Preview.html")
         tidied_sheets.append(c1.topandas())
         
+        new_table_1_2 = pd.concat(tidied_sheets, ignore_index = True, sort = True).fillna('')
+        
+        tidy_tabs[tab_name] = new_table_1_2       
         
     elif tab.name in ['1_3']:
+        
+        tab_name = 'Number of Starts and Job Outcomes by Start month and Referral Group'
         
         cell = tab.filter("In month")
         
@@ -104,11 +115,11 @@ for tab in tabs:
         
         period = cell.shift(2,0).fill(DOWN).is_not_blank().shift(-2,0) - remove 
 
-        referralGroup = cell.shift(5,0).fill(RIGHT).is_not_blank()
+        referralGroup = cell.fill(RIGHT).is_not_blank()
         
         obsType = cell.shift(1,-1).fill(RIGHT).is_not_blank()
 
-        observations = period.shift(5,0).fill(RIGHT).is_not_blank() - remove
+        observations = period.fill(RIGHT).is_not_blank() - remove
 
         dimensions = [
                 HDim(period, 'Period', DIRECTLY, LEFT), 
@@ -126,8 +137,13 @@ for tab in tabs:
         savepreviewhtml(c1, fname="Preview.html")
         tidied_sheets.append(c1.topandas())
         
+        new_table_1_3 = pd.concat(tidied_sheets, ignore_index = True, sort = True).fillna('')
+        
+        tidy_tabs[tab_name] = new_table_1_3
         
     elif tab.name in ['1_4']:
+        
+        tab_name = 'Job Outcomes by Job Outcome month'
         
         cell = tab.filter("In month")
         
@@ -156,7 +172,13 @@ for tab in tabs:
         savepreviewhtml(c1, fname="Preview.html")
         tidied_sheets.append(c1.topandas())
         
+        new_table_1_4 = pd.concat(tidied_sheets, ignore_index = True, sort = True).fillna('')
+        
+        tidy_tabs[tab_name] = new_table_1_4
+        
     elif tab.name in ['2_1']:
+        
+        tab_name = 'Number of Referrals, Individuals referred and Starts, by Contract Package Area, Local Government Partnership and Referral Group'
         
         cell = tab.filter("Disability1")
         
@@ -185,7 +207,13 @@ for tab in tabs:
         savepreviewhtml(c1, fname="Preview.html")
         tidied_sheets.append(c1.topandas())
         
+        new_table_2_1 = pd.concat(tidied_sheets, ignore_index = True, sort = True).fillna('')
+        
+        tidy_tabs[tab_name] = new_table_2_1
+        
     elif tab.name in ['2_2']:
+        
+        tab_name = 'Referrals, Individual Referrals and Starts, by Devolved Deal Area'
         
         cell = tab.filter("In Month")
         
@@ -216,7 +244,13 @@ for tab in tabs:
         savepreviewhtml(c1, fname="Preview.html")
         tidied_sheets.append(c1.topandas())
         
+        new_table_2_2 = pd.concat(tidied_sheets, ignore_index = True, sort = True).fillna('')
+        
+        tidy_tabs[tab_name] = new_table_2_2
+        
     elif tab.name in ['3_1']:
+        
+        tab_name = 'Number of Starts, by age by gender'
         
         cell = tab.filter("3.1").shift(2,5)
         
@@ -243,7 +277,13 @@ for tab in tabs:
         savepreviewhtml(c1, fname="Preview.html")
         tidied_sheets.append(c1.topandas())
         
+        new_table_3_1 = pd.concat(tidied_sheets, ignore_index = True, sort = True).fillna('')
+        
+        tidy_tabs[tab_name] = new_table_3_1
+        
     elif tab.name in ['3_2']:
+        
+        tab_name = 'Number of Starts, by Age and Contract Package Area, Local Government Partnership'
         
         cell = tab.filter("3.2").shift(2,5)
         
@@ -272,7 +312,13 @@ for tab in tabs:
         savepreviewhtml(c1, fname="Preview.html")
         tidied_sheets.append(c1.topandas())
         
+        new_table_3_2 = pd.concat(tidied_sheets, ignore_index = True, sort = True).fillna('')
+        
+        tidy_tabs[tab_name] = new_table_3_2
+        
     elif tab.name in ['3_3']:
+        
+        tab_name = 'Starts, by gender'
         
         cell = tab.filter("In month")
         
@@ -280,13 +326,11 @@ for tab in tabs:
         
         remove = period.filter('Total').expand(RIGHT)
         
-        remove2 = cell.shift(4,0).fill(DOWN)
-        
         obsType = cell.shift(4,-1).expand(RIGHT).is_not_blank()
         
-        gender = cell.shift(RIGHT).expand(RIGHT).is_not_blank() - remove2
+        gender = cell.shift(RIGHT).expand(RIGHT).is_not_blank()
 
-        observations = cell.shift(1,2).expand(DOWN).expand(RIGHT).is_not_blank() - remove - remove2
+        observations = cell.shift(1,2).expand(DOWN).expand(RIGHT).is_not_blank() - remove
 
         dimensions = [
                 HDim(period, 'Period', DIRECTLY, LEFT), 
@@ -303,105 +347,87 @@ for tab in tabs:
         c1 = ConversionSegment(observations, dimensions, processTIMEUNIT=True)
         savepreviewhtml(c1, fname="Preview.html")
         tidied_sheets.append(c1.topandas())
+        break
+        
+        new_table_3_3 = pd.concat(tidied_sheets, ignore_index = True, sort = True).fillna('')
+        
+        tidy_tabs[tab_name] = new_table_3_3
         
     else:
         continue
 
 
-# In[81]:
+# In[53]:
 
 
-new_table = pd.concat(tidied_sheets, ignore_index = True, sort = True).fillna('')
+for i in tidy_tabs:
+    new_table = tidy_tabs.get(i)
+    
+    new_table.rename(columns={'OBS': 'Value'}, inplace=True)
+    new_table['Period'] = new_table['Period'].astype(str)
 
-new_table.rename(columns={'OBS': 'Value'}, inplace=True)
-new_table['Period'] = new_table['Period'].astype(str)
+    new_table['Period'] = new_table['Period'].map(lambda x: 'gregorian-interval/2017-11-01T00:00:00/P22M' if 'inclusive' in x else 'gregorian-interval/' + left(x,4) + '-' + mid(x,5,2) + '-01T00:00:00/P1M')
 
-new_table['Period'] = new_table['Period'].map(lambda x: 'gregorian-interval/2017-11-01T00:00:00/P22M' if 'inclusive' in x else 'gregorian-interval/' + left(x,4) + '-' + mid(x,5,2) + '-01T00:00:00/P1M')
+    new_table['Age Group'] = new_table['Age Group'].map(lambda x: x.replace('Aged ', ''))
+    new_table['Age Group'] = new_table['Age Group'].map(lambda x: x.replace('+', ' plus'))
+    new_table['Region'] = new_table['Region'].map(lambda x: left(x, len(x) -1) if x.endswith('1') else x)
+    new_table['Region'] = new_table['Region'].map(lambda x: left(x, len(x) -1) if x.endswith('3') else x) 
+    
+    tidy = new_table[['Period','Region','Age Group','Gender','Referral Group','Observation Type','Measure Type','Value','DATAMARKER','Unit']]
+    tidy = tidy.replace({'Referral Group' : {
+        'Disability1' : 'Disability', 
+        'Early Access1' : 'Early Access', 
+        'Long Term Unemployed2' : 'Long Term Unemployed',
+        'Total' : 'all'}})
+    tidy = tidy.replace({'Observation Type' : {
+        'Individuals Referred 3' : 'Individuals Referred',
+        'Individuals Referred4' : 'Individuals Referred',
+        'Individual Referrals 1' : 'Individuals Referred',
+        'Job Outcome 2' : 'Job Outcome',
+        'total' : 'all'}})
+    tidy = tidy.replace({'DATAMARKER' : {
+        '-' : 'not-applicable', 
+        '.' : 'negligible'}})
+    tidy = tidy.replace({'Age Group' : {
+        'Unknown Age' : 'unknown',
+        'Total' : 'all'}})
 
-new_table['Age Group'] = new_table['Age Group'].map(lambda x: x.replace('Aged ', ''))
-new_table['Age Group'] = new_table['Age Group'].map(lambda x: x.replace('+', ' plus'))
-new_table['Region'] = new_table['Region'].map(lambda x: left(x, len(x) -1) if x.endswith('1') else x)
-new_table['Region'] = new_table['Region'].map(lambda x: left(x, len(x) -1) if x.endswith('3') else x) 
+    tidy.rename(columns={'DATAMARKER' : 'Marker',
+                         'Region' : 'DWP Area',
+                         'Gender' : 'Sex',
+                         'Age Group' : 'DWP Age Group',
+                         'Observation Type' : 'DWP Referral Type',
+                         'Referral Group' : 'DWP Referral Group'}, inplace=True)
 
+    tidy['DWP Referral Group'] = tidy['DWP Referral Group'].map(lambda x: pathify(x))
+    tidy['DWP Referral Type'] = tidy['DWP Referral Type'].map(lambda x: pathify(x))
+    tidy['DWP Area'] = tidy['DWP Area'].map(lambda x: pathify(x))
+    tidy['Sex'] = tidy['Sex'].map(lambda x: pathify(x))
+    tidy['DWP Age Group'] = tidy['DWP Age Group'].map(lambda x: pathify(x))
+    #tidy = tidy.replace({'Area' : {'england-and-wales' : 'K04000001'}})
 
-# In[82]:
+    tidy = tidy.replace({'Sex' : {
+        'all' : 'T', 
+        'female' : 'F', 
+        'females' : 'F', 
+        'male' : 'M', 
+        'males' : 'M', 
+        'unknown' : 'T'}})
+    
+    destinationFolder = Path('out')
+    destinationFolder.mkdir(exist_ok=True, parents=True)
 
+    TAB_NAME = i
+    
+    tidy.drop_duplicates().to_csv(destinationFolder / f'{TAB_NAME}.csv', index = False)
+    
+    scraper.dataset.family = 'disability'
+    #scraper.dataset.theme = THEME['health-social-care']
+    with open(destinationFolder / 'observations.csv-metadata.trig', 'wb') as metadata:
+        metadata.write(scraper.generate_trig())
 
-tidy = new_table[['Period','Region','Age Group','Gender','Referral Group','Observation Type','Measure Type','Value','DATAMARKER','Unit']]
-tidy = tidy.replace({'Referral Group' : {
-    'Disability1' : 'Disability', 
-    'Early Access1' : 'Early Access', 
-    'Long Term Unemployed2' : 'Long Term Unemployed',
-    'Total' : 'all'}})
-tidy = tidy.replace({'Observation Type' : {
-    'Individuals Referred 3' : 'Individuals Referred',
-    'Individuals Referred4' : 'Individuals Referred',
-    'Individual Referrals 1' : 'Individuals Referred',
-    'Job Outcome 2' : 'Job Outcome',
-    'total' : 'all'}})
-tidy = tidy.replace({'DATAMARKER' : {
-    '-' : 'not-applicable', 
-    '.' : 'negligible'}})
-tidy = tidy.replace({'Age Group' : {
-    'Unknown Age' : 'unknown',
-    'Total' : 'all'}})
-
-tidy.rename(columns={'DATAMARKER' : 'Marker',
-                     'Region' : 'DWP Area',
-                     'Gender' : 'Sex',
-                     'Age Group' : 'DWP Age Group',
-                     'Observation Type' : 'DWP Referral Type',
-                     'Referral Group' : 'DWP Referral Group'}, inplace=True)
-
-tidy['DWP Referral Group'] = tidy['DWP Referral Group'].map(lambda x: pathify(x))
-tidy['DWP Referral Type'] = tidy['DWP Referral Type'].map(lambda x: pathify(x))
-tidy['DWP Area'] = tidy['DWP Area'].map(lambda x: pathify(x))
-tidy['Sex'] = tidy['Sex'].map(lambda x: pathify(x))
-tidy['DWP Age Group'] = tidy['DWP Age Group'].map(lambda x: pathify(x))
-#tidy = tidy.replace({'Area' : {'england-and-wales' : 'K04000001'}})
-
-tidy = tidy.replace({'Sex' : {
-    'all' : 'T', 
-    'female' : 'F', 
-    'females' : 'F', 
-    'male' : 'M', 
-    'males' : 'M', 
-    'unknown' : 'T'}})
-
-
-# In[83]:
-
-
-from IPython.core.display import HTML
-for col in tidy:
-    if col not in ['Value']:
-        tidy[col] = tidy[col].astype('category')
-        display(HTML(f"<h2>{col}</h2>"))
-        display(tidy[col].cat.categories)    
-
-
-# In[84]:
-
-
-destinationFolder = Path('out')
-destinationFolder.mkdir(exist_ok=True, parents=True)
-
-TAB_NAME = 'observations'
-
-tidy.drop_duplicates().to_csv(destinationFolder / f'{TAB_NAME}.csv', index = False)
-tidy    
-
-
-# In[85]:
-
-
-scraper.dataset.family = 'disability'
-#scraper.dataset.theme = THEME['health-social-care']
-with open(destinationFolder / 'observations.csv-metadata.trig', 'wb') as metadata:
-    metadata.write(scraper.generate_trig())
-
-csvw = CSVWMetadata('https://gss-cogs.github.io/family-disability/reference/')
-csvw.create(destinationFolder / 'observations.csv', destinationFolder / 'observations.csv-schema.json')
+    csvw = CSVWMetadata('https://gss-cogs.github.io/family-disability/reference/')
+    csvw.create(destinationFolder / f'{TAB_NAME}.csv', destinationFolder / f'{TAB_NAME}.csv-schema.json')
 
 
 # In[ ]:
